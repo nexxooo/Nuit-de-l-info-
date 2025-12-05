2c095b0ad488f0c39be05ea8f2ff6597a15ca4e0
Kevin Vasilescu <enola64@gmail.com>
2025-12-05 04:21:37 +0100

    changement et première page qui marche

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-const question1 = document.getElementById("questio");
+const intro = document.getElementById("intro");
+const introEtape1 = document.getElementById("introEtape1");
+const level1 = document.getElementById("level1");
+const level2 = document.getElementById("level2");
+const level3 = document.getElementById("level3");
+
+const etape1 = document.getElementById("etape1");
+
+const btnEntrerJeu = document.getElementById("enterGame");
+
+// intro.classList.add("hide");
+introEtape1.classList.add("hide");
+level1.classList.add("hide");
+level2.classList.add("hide");
+level3.classList.add("hide");
+etape1.classList.add("etape1");
+
+btnEntrerJeu.addEventListener("click", () => {
+  introEtape1.classList.remove("hide");
+});
